3ad901c8ac6bd3dbece33abbeee16ab6da48b3c5
Łukasz Kleczaj <luke.clechay@gmail.com>
2018-06-25 14:52:39 +0200

    rmr encapsulation

---
+++ src/rm/rm.ts
@@ -0,0 +1,10 @@
+import { Engine } from './engine'
+import { Firmware } from './firmware'
+import { State } from './state'
+
+class RM{
+    engine : Engine;
+    firmware : Firmware;
+    state_ : State;
+
+}
